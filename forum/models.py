dc6bbab9a515ff76931495c359aff710c061cf33
unknown <jasonirvin08@gmail.com>
2019-05-04 16:17:33 +0800

    Like Button

--- forum/models.py
+++ forum/models.py
@@ -8,10 +8,14 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    like = models.ManyToManyField(User, related_name='likes', blank=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+    def total_likes(self):
+        return self.like.count()
 
