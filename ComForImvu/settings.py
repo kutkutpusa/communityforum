2a1e63420238da286d50a5fed300c9f180dd3071
Jhoda P. Embodo <jhodalouise@gmail.com>
2019-05-16 20:55:55 +0800

    Update

--- ComForImvu/settings.py
+++ ComForImvu/settings.py
@@ -23,7 +23,14 @@ EMAIL_PORT = 587
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+SETTINGS_PATH = os.path.dirname(__file__)
+PROJECT_PATH = os.path.join(SETTINGS_PATH, os.pardir)
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+TEMPLATES_PATH = os.path.join(PROJECT_PATH, "templates")
 
+TEMPLATE_DIRS = (
+    TEMPLATES_PATH,
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -75,7 +82,7 @@ ROOT_URLCONF = 'ComForImvu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIRS,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,14 +148,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-SETTINGS_PATH = os.path.dirname(__file__)
-PROJECT_PATH = os.path.join(SETTINGS_PATH, os.pardir)
-PROJECT_PATH = os.path.abspath(PROJECT_PATH)
-TEMPLATES_PATH = os.path.join(PROJECT_PATH, "templates")
 
-TEMPLATE_DIRS = (
-    TEMPLATES_PATH,
-)
 #STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, 'static'),
 #)
