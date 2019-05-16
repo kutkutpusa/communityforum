fca9ea61716d5b0dd069d9326c272f81f3d270cb
Jhoda P. Embodo <jhodalouise@gmail.com>
2019-05-16 17:06:36 +0800

    Update

--- ComForImvu/settings.py
+++ ComForImvu/settings.py
@@ -23,6 +23,9 @@ EMAIL_PORT = 587
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
