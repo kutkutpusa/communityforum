63aefa9f3aaf368cc63018db3dce09636e3254e9
Jhoda P. Embodo <jhodalouise@gmail.com>
2019-05-16 19:39:30 +0800

    Update

--- ComForImvu/settings.py
+++ ComForImvu/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['imvucommunityforum.herokuapp.com']
 
 
 # Application definition
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'ComForImvu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, "templates"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
