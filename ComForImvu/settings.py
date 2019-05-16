09a21c8c6227f831794ebc6f6954eaf7fe293ba7
Jhoda P. Embodo <jhodalouise@gmail.com>
2019-05-16 18:25:58 +0800

    Updates

--- ComForImvu/settings.py
+++ ComForImvu/settings.py
@@ -22,7 +22,7 @@ EMAIL_PORT = 587
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MAIN_DIR = os.path.dirname(os.path.dirname(__file__))) #for heroku
+#MAIN_DIR = os.path.dirname(os.path.dirname(__file__))) #for heroku
 
 
 # Quick-start development settings - unsuitable for production
@@ -70,7 +70,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(MAIN_DIR, 'templates')
+            os.path.join(BASE_DIR, 'templates')
             ],
         'APP_DIRS': True,
         'OPTIONS': {
