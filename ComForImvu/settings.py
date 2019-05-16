bc69509aa1fa4e6af8904e1a98ea38f61a3936c6
Jhoda P. Embodo <jhodalouise@gmail.com>
2019-05-17 06:27:09 +0800

    Update

--- ComForImvu/settings.py
+++ ComForImvu/settings.py
@@ -84,7 +84,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'templates'),
+            os.path.join(BASE_DIR,'forum/templates/Forum/main.html'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
