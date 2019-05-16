f2b0c5ca3c3536378c81eb61fee57360f8365ca1
Jhoda P. Embodo <jhodalouise@gmail.com>
2019-05-16 13:27:40 +0800

    Updated Settings

--- ComForImvu/settings.py
+++ ComForImvu/settings.py
@@ -27,12 +27,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2eh2v&hjd*)c57kojvln(qpy&v7g^35c=oa03d+kf_avp9s5n-'
+# SECRET_KEY = '2eh2v&hjd*)c57kojvln(qpy&v7g^35c=oa03d+kf_avp9s5n-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['imvucommunityforum.herokuapp.com']
 
 
 # Application definition
